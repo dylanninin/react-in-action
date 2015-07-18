48c75379669ec3cf8c50a62a99961760873f5fd3
Dylan <dylanninin@gmail.com>
2015-07-18 12:46:23 +0800

    Update handleCommentSubmit

--- src/comments/comments.js
+++ src/comments/comments.js
@@ -31,8 +31,18 @@ var CommentBox = React.createClass({
         });
     },
     handleCommentSubmit: function(comment){
-        // TODO: submit to the server and refresh the list
-        console.log(comment)
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
+            success: function(data){
+                this.setState({data:data});
+            }.bind(this),
+            error: function(xhr, status, err){
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
     },
     getInitialState: function(){
         return {data: []};
