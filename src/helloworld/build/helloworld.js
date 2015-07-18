3939f0abd8efcdde9f72dbe04b349f2b61ea4c14
Dylan <dylanninin@gmail.com>
2015-07-18 11:41:25 +0800

    Add HelloWorld

---
+++ src/helloworld/build/helloworld.js
@@ -0,0 +1,7 @@
+/**
+ * Created by dylanninin on 7/18/15.
+ */
+React.render(
+    React.createElement("h1", null, "Hello, world!") ,
+    document.getElementById('example')
+);
---
+++ src/helloworld/helloworld.js
@@ -0,0 +1,7 @@
+/**
+ * Created by dylanninin on 7/18/15.
+ */
+React.render(
+    <h1>Hello, world!</h1> ,
+    document.getElementById('example')
+);
